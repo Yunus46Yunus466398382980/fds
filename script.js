4a3ea24e7660a50b294ee9663305b84797c379db
Yunus46Yunus466398382980 <munojat.1585@gmail.com>
2025-06-28 01:04:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+const fruits = ['olma', 'Banan', 'Gilos', 'Shaftoli']
+console.log(fruits)
+
+
+alert("Boshlanishida Arryning uzunligi 4")
+
+
+let question = confirm("sizning arrayingizdan malumotlarni olib tashlamoqchimisiz")
+
+alert("Arrayingiz uzunligi 3")
+
+let sorov
+let sovol = confirm('array dan bitta yozuv ni ochiramizmi?')
+if (sovol) {
+let ism = "(3)[ 'olma', 'Banan', 'Gilos']";
+console.log(ism)
+let mevalar = mevalar.pop();
+}
+else {
+    console.log(fruits)
+}
